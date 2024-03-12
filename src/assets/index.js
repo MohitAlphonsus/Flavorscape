cdca03d931d06323256626832fec206975c8eee9
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-12 22:29:02 +0530

    images are imported

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,22 @@ import aboutImgFirst from './about-1.jpg';
 import aboutImgSecond from './about-2.jpg';
 import chefPotrait from './chefDraw.png';
 
-export { heroImg, aboutImgFirst, aboutImgSecond, chefPotrait };
+import customer_1 from './customers/customer-1.jpg';
+import customer_2 from './customers/customer-2.jpg';
+import customer_3 from './customers/customer-3.jpg';
+import customer_4 from './customers/customer-4.jpg';
+import customer_5 from './customers/customer-5.jpg';
+import customer_6 from './customers/customer-6.jpg';
+
+export {
+	heroImg,
+	aboutImgFirst,
+	aboutImgSecond,
+	chefPotrait,
+	customer_1,
+	customer_2,
+	customer_3,
+	customer_4,
+	customer_5,
+	customer_6,
+};
