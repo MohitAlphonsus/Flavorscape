65eab03e2c1cf755a00533ccee87d560829fb5cd
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-12 22:29:28 +0530

    components for testimonial section

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TestimonialsStyle = styled.section`
+	height: 100vh;
+`;
+
+function Testimonials() {
+	return <TestimonialsStyle>Testimonials</TestimonialsStyle>;
+}
+
+export default Testimonials;
