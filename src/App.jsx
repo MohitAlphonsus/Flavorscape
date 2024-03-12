62e17eddcdf5d10badb558d1f972ccb6c5a88c9a
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-12 23:20:00 +0530

    testimonial component file added/imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import GlobalStyles from './UI/GlobalStyles';
 import { Header, About } from './components';
+import Testimonials from './components/Testimonials';
 
 export default function App() {
 	return (
 		<>
 			<Header />
 			<About />
-
+			<Testimonials />
 			<GlobalStyles />
 		</>
 	);
