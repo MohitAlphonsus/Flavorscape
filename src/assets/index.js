8ce0c00cdc6335fe4b4bcad4ef6802442bfcc3ab
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-12 21:54:06 +0530

    image imported

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import heroImg from './hero.jpg';
 import aboutImgFirst from './about-1.jpg';
 import aboutImgSecond from './about-2.jpg';
+import chefPotrait from './chefDraw.png';
 
-export { heroImg, aboutImgFirst, aboutImgSecond };
+export { heroImg, aboutImgFirst, aboutImgSecond, chefPotrait };
