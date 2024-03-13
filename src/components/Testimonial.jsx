fe2d595fe3c4f99b63e542d0c328504e7c17e948
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-13 15:19:14 +0530

    testimonial section created and styled

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,66 @@
-function Testimonial() {
-	return <div>Testimonial</div>;
+import styled from 'styled-components';
+import { SmallText } from '../UI/Typo';
+
+const TestimonialStyle = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+
+	color: var(--color-accent-white);
+	background-color: rgba(255, 255, 255, 0.1);
+	padding: 2rem 4rem;
+	border-radius: 9px;
+
+	&:last-child {
+		grid-column: span 2;
+	}
+`;
+
+const Customer = styled.div`
+	display: grid;
+	align-items: center;
+	column-gap: 1rem;
+	grid-template-columns: min-content 1fr;
+
+	div {
+		grid-row: 1 / -3;
+		height: 6rem;
+		width: 6rem;
+		border-radius: 50%;
+		background: url(${props => props.customerphoto}) no-repeat center
+			center/cover;
+	}
+
+	p {
+		font-size: 1.4rem;
+		text-align: left;
+	}
+
+	span {
+		display: inline-block;
+		font-size: 1.2rem;
+		font-style: italic;
+		opacity: 0.9;
+		grid-column: 2 / -1;
+	}
+`;
+
+function Testimonial({
+	customerName,
+	dishName,
+	testimonialText,
+	customerphoto,
+}) {
+	return (
+		<TestimonialStyle>
+			<Customer customerphoto={customerphoto}>
+				<div role="image">&nbsp;</div>
+				<p>{customerName}</p>
+				<span>(Review for {dishName})</span>
+			</Customer>
+			<SmallText>{testimonialText}</SmallText>
+		</TestimonialStyle>
+	);
 }
 
 export default Testimonial;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,64 @@
 import styled from 'styled-components';
+import Testimonial from './Testimonial';
+import { SecondaryHeading, SubHeading } from '../UI/Typo';
+import { testimonialBg } from '../assets';
+import { testimonialData } from '../constants/testimonialData';
 
 const TestimonialsStyle = styled.section`
-	height: 100vh;
+	background-image: linear-gradient(
+			to right,
+			rgba(0, 0, 0, 0.9),
+			rgba(0, 0, 0, 0.9)
+		),
+		url(${testimonialBg});
+	background-attachment: fixed;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 6rem 2rem;
+	color: var(--color-accent-white);
+`;
+
+const TestimonialContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	gap: 2rem;
+	width: min(95%, 120rem);
+`;
+
+const Heading = styled.div`
+	text-align: center;
+	margin-bottom: 6rem;
 `;
 
 function Testimonials() {
-	return <TestimonialsStyle>Testimonials</TestimonialsStyle>;
+	return (
+		<TestimonialsStyle>
+			<Heading>
+				<SubHeading>Rave Reviews from Our Esteemed Guests</SubHeading>
+				<SecondaryHeading>
+					Discover What Our Customers Have to Say About Their Culinary
+					Experience
+				</SecondaryHeading>
+			</Heading>
+			<TestimonialContainer>
+				{testimonialData.map(data => (
+					<Testimonial
+						key={data.id}
+						customerName={data.customerName}
+						dishName={data.dish}
+						testimonialText={data.testimonial}
+						customerphoto={data.customerPhoto}
+					/>
+				))}
+			</TestimonialContainer>
+		</TestimonialsStyle>
+	);
 }
 
 export default Testimonials;
