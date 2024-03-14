5ce468442d0955cfad7b237acd35ac6013a862bb
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-14 21:46:22 +0530

    fetures section constants

---
+++ src/constants/features.js
@@ -0,0 +1,49 @@
+import {
+	FaTruckFast,
+	FaBellConcierge,
+	FaMusic,
+	FaChampagneGlasses,
+	FaLeaf,
+	FaInfinity,
+} from 'react-icons/fa6';
+
+const features = [
+	{
+		id: 1,
+		heading: 'Effortless Ordering',
+		desc: 'Experience the convenience of ordering your favorite dishes from the comfort of your home.',
+		icon: FaTruckFast,
+	},
+	{
+		id: 2,
+		heading: 'Private Dining',
+		desc: "Elevate your special occasions with our exclusive private dining options. Whether it's a family celebration or a corporate event.",
+		icon: FaBellConcierge,
+	},
+	{
+		id: 3,
+		heading: 'Sustainable Practices',
+		desc: 'From sourcing local ingredients to minimizing waste, we strive to minimize our environmental footprint while delivering exceptional dining experiences',
+		icon: FaLeaf,
+	},
+	{
+		id: 4,
+		heading: 'Cultural Fusion',
+		desc: 'Experience a harmonious blend of cultures and cuisines, where diverse influences come together.',
+		icon: FaInfinity,
+	},
+	{
+		id: 5,
+		heading: 'Fine Spirits',
+		desc: '"Unwind and indulge in handcrafted cocktails and premium spirits at our elegant cocktail bar.',
+		icon: FaChampagneGlasses,
+	},
+	{
+		id: 6,
+		heading: 'Music Evenings',
+		desc: 'Enhance your dining experience with live music performances that set the perfect ambiance.',
+		icon: FaMusic,
+	},
+];
+
+export { features };
