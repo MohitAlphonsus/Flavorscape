758689e810c41052a4062af5791bf7af93d92dcc
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-15 12:02:17 +0530

    dishes object added

--- src/constants/dishes.js
+++ src/constants/dishes.js
@@ -1,8 +1,53 @@
+import { dish1, dish2, dish3, dish4, dish5, dish6 } from '../assets';
 const dishes = [
 	{
 		id: 1,
 		name: 'Tandoori Chicken',
 		ingredients:
 			'Tender chicken marinated in a blend of yogurt, ginger, garlic, and aromatic spices, cooked to perfection in a traditional tandoor oven.',
+		imgUrl: dish1,
+		price: 350,
+	},
+	{
+		id: 2,
+		name: 'Paneer Tikka Masala',
+		ingredients:
+			'Cubes of paneer (Indian cottage cheese) marinated in a creamy yogurt sauce, grilled to perfection, and simmered in a rich tomato and onion gravy.',
+		imgUrl: dish2,
+		price: 320,
+	},
+	{
+		id: 3,
+		name: 'Butter Naan',
+		ingredients:
+			'Soft and fluffy bread made from refined flour, butter, and yogurt, baked in a clay oven until golden brown.',
+		imgUrl: dish3,
+		price: 70,
+	},
+	{
+		id: 4,
+		name: 'Rogan Josh',
+		ingredients:
+			'Tender pieces of lamb cooked in a flavorful gravy made with aromatic spices like cardamom, cloves, and cinnamon, finished with a touch of saffron-infused yogurt.',
+		imgUrl: dish4,
+		price: 420,
+	},
+	{
+		id: 5,
+		name: 'Palak Paneer',
+		ingredients:
+			'Fresh spinach leaves cooked with cubes of paneer (Indian cottage cheese), onions, tomatoes, and a blend of aromatic spices.',
+		imgUrl: dish5,
+		price: 300,
+	},
+	{
+		id: 6,
+		name: 'Masala Dosa',
+		ingredients:
+			'Thin and crispy rice and lentil crepe filled with a spicy potato filling, served with coconut chutney and sambar (spiced lentil soup).',
+		imgUrl: dish6,
+		price: 250,
 	},
 ];
+
+export { dishes };
