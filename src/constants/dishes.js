3799421f48f40db70920250e32c7af7fd0811d85
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-03-15 11:56:22 +0530

    constant object created for dishes

---
+++ src/constants/dishes.js
@@ -0,0 +1,8 @@
+const dishes = [
+	{
+		id: 1,
+		name: 'Tandoori Chicken',
+		ingredients:
+			'Tender chicken marinated in a blend of yogurt, ginger, garlic, and aromatic spices, cooked to perfection in a traditional tandoor oven.',
+	},
+];
